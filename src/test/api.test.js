64e16031187149e9f93cb065ed2bdc04e905e734
Franco <97957054+Francooo0@users.noreply.github.com>
2023-04-21 11:00:10 +0300

    Unit tests for my API (for each endpoint)

---
+++ src/test/api.test.js
@@ -0,0 +1,93 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const loginUser = async () => {
+    const res = await chai
+        .request(server)
+        .post("/login")
+        .send({ email: "test@example.com", password: "password" });
+    return res.body.token;
+};
+
+describe("API", () => {
+    let token;
+    let createdPhotoId;
+
+    before(async () => {
+        token = await loginUser();
+    });
+
+    describe("GET /photos", () => {
+        it("should GET all photos", (done) => {
+            chai.request(server)
+                .get("/photos")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("array");
+                    done();
+                });
+        });
+    });
+
+    describe("GET /photos/:id", () => {
+        it("should GET a photo by the given id", (done) => {
+            const id = 1;
+            chai.request(server)
+                .get("/photos/" + id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("id").eql(id);
+                    done();
+                });
+        });
+    });
+
+    it("should POST a new photo", async () => {
+        const photo = {
+            title: "Test Photo",
+            url: "https://example.com/test.jpg",
+        };
+        const res = await chai
+            .request(server)
+            .post("/photos")
+            .set("Authorization", `Bearer ${token}`)
+            .send(photo);
+        res.should.have.status(201);
+        res.body.should.be.a("object");
+        res.body.should.have.property("title").eql(photo.title);
+        res.body.should.have.property("url").eql(photo.url);
+
+        createdPhotoId = res.body.id;
+    });
+
+    it("should UPDATE a photo by the given id", async () => {
+        const updatedPhoto = {
+            title: "Updated Test Photo",
+            url: "https://example.com/updated-test.jpg",
+        };
+        const res = await chai
+            .request(server)
+            .put(`/photos/${createdPhotoId}`)
+            .set("Authorization", `Bearer ${token}`)
+            .send(updatedPhoto);
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("title").eql(updatedPhoto.title);
+        res.body.should.have.property("url").eql(updatedPhoto.url);
+    });
+
+    it("should DELETE a photo by the given id", async () => {
+        const res = await chai
+            .request(server)
+            .delete(`/photos/${createdPhotoId}`)
+            .set("Authorization", `Bearer ${token}`);
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("message").eql("Photo deleted successfully");
+    });
+});
